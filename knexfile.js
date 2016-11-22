ebcde5ad01186c40a65b5d156bcbe33e2f3287b8
Micah Britton <micahwkb@gmail.com>
2016-11-22 18:00:51 -0500

    cleaned up knexfile

--- knexfile.js
+++ knexfile.js
@@ -2,12 +2,6 @@
 const settings = require("./settings");
 module.exports = {
 
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
 
   staging: {
     client: 'postgresql',
